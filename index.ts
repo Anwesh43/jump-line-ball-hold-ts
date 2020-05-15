439a4dcaf2ce5fd1e35e3757b2b527c95f19bf14
Anwesh43 <anweshthecool0@gmail.com>
2020-05-15 21:28:36 +0530

    adding JumpingBallLineHolder class

--- index.ts
+++ index.ts
@@ -186,3 +186,27 @@ class JLBNode {
         return this
     }
 }
+
+class JumpingBallLineHolder {
+
+    root : JLBNode = new JLBNode(0)
+    curr : JLBNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, ()=> {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
