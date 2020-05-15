641785151dbcf577c6314cefe450f5557fea20b5
Anwesh43 <anweshthecool0@gmail.com>
2020-05-15 20:45:57 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,10 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.02
+const strokeFactor : number = 90
+const sizeFactor : number = 2.9
+const foreColor : string = "#3F51B5"
+const backColor : string = "#BDBDBD"
+const delay : number = 20
+const nodes : number = 5
+const parts : number = 2
