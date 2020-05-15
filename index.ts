537007353c3cb8ff1be2cf98df70e31a6849b377
Anwesh43 <anweshthecool0@gmail.com>
2020-05-15 21:26:16 +0530

    adding JLBNode class

--- index.ts
+++ index.ts
@@ -141,3 +141,48 @@ class Animator {
         }
     }
 }
+
+class JLBNode {
+
+    next : JLBNode
+    prev : JLBNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new JLBNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawJLBNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : JLBNode {
+        var curr : JLBNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
