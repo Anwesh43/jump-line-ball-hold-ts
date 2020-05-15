d9fe1c959ebf21a4c838df66fe6ff484ffe7680a
Anwesh43 <anweshthecool0@gmail.com>
2020-05-15 21:16:00 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ const backColor : string = "#BDBDBD"
 const delay : number = 20
 const nodes : number = 5
 const parts : number = 2
+const rFactor : number = 3.3
 
 class ScaleUtil {
 
@@ -21,6 +22,48 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawRotateLineJumpingCircle(context : CanvasRenderingContext2D, h : number, scale : number, size : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, 2)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, 2)
+        context.save()
+        context.translate(0, h / 2)
+        context.rotate(sf1 * Math.PI / 2)
+        DrawingUtil.drawLine(context, 0, 0, 0, -size)
+        context.restore()
+        const r : number = size / rFactor
+        DrawingUtil.drawCircle(context, w / 2, r + (h - 2 * r) * sf2, r)
+    }
+
+    static drawJLBNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / sizeFactor
+        context.strokeStyle = foreColor
+        context.fillStyle = foreColor
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        context.save()
+        context.translate(gap  * (i + 1), h)
+        DrawingUtil.drawRotateLineJumpingCircle(context, h, scale, size)
+        context.restore()
     }
 }
 
