e9a3569f4891ee018b418f6d68e550e8482f21cb
Anwesh43 <anweshthecool0@gmail.com>
2020-05-15 20:50:50 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -9,6 +9,21 @@ const delay : number = 20
 const nodes : number = 5
 const parts : number = 2
 
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
+
 class Stage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
